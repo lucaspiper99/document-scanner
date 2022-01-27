a6ed5cece98a86e4b3a406f548904fdc8c06ef1f
afonso-g-martins <afonso.girbal.martins@tecnico.ulisboa.pt>
2022-01-27 21:21:54 +0000

    PART2 DONEEEEE

--- compare2.m
+++ compare2.m
@@ -4,12 +4,12 @@ hists = zeros(size(rgb_images, 4),256);
 
 for i = 1:size(rgb_images, 4)
     imageO_ = rgb_images(:,:,:,i);
-    hist_val = imhist(rgb2val(imageO_));
+    hist_val = imhist(rgb2hue(imageO_));
     hists(i,:) = hist_val./sum(hist_val);
 end
 
-corrs = zeros(size(rgb_images, 4));
-minsum = 100000000000;
+%corrs = zeros(size(rgb_images, 4));
+minsum = 10000000000000000;
 
 sums = zeros(size(rgb_images, 4),1);
 for i = 1:size(rgb_images, 4)
@@ -17,16 +17,18 @@ for i = 1:size(rgb_images, 4)
     for j = 1:size(rgb_images, 4)
         histval = dist_kl(hists(i,:),hists(j,:));
         dist_sum = dist_sum + histval;
-        corrs(i,j) = histval;
+        %corrs(i,j) = histval;
     end
     sums(i) = dist_sum;
     if dist_sum < minsum
         minsum = dist_sum;
     end
 end
+%sums
 
-[goodim, goodent] = mink(sums,round(length(sums)/4));
-minsum = 10000000000;
+num_pass = sum(sums<=min(sums)*1.8);
+[goodim, goodent] = mink(sums,num_pass);
+minsum = 100000000000000000;
 sums2 = zeros(length(goodent),1);
 
 for i = 1:length(goodent)
@@ -34,15 +36,39 @@ for i = 1:length(goodent)
     for j = 1:length(goodent)
         histval = dist_kl(hists(goodent(i),:),hists(goodent(j),:));
         dist_sum = dist_sum + histval;
-        corrs(i,j) = histval;
+        %corrs(i,j) = histval;
     end
     sums2(i) = dist_sum;
+    if dist_sum < minsum
+        minsum = dist_sum;
+        FINAL1 = i;
+    end
+end
+
+
+
+num_pass2 = sum(sums2<=min(sums2)*1.2);
+[goodim2, goodent2] = mink(sums,num_pass2);
+minsum = 100000000000000000;
+sums3 = zeros(length(goodent2),1);
+
+for i = 1:length(goodent2)
+    dist_sum = 0;
+    for j = 1:length(goodent2)
+        histval = dist_kl(hists(goodent(i),:),hists(goodent(j),:));
+        dist_sum = dist_sum + histval;
+        %corrs(i,j) = histval;
+    end
+    sums3(i) = dist_sum;
     if dist_sum < minsum
         minsum = dist_sum;
         FINAL = i;
     end
 end
-final_index = goodent(FINAL);
+
+
+
+final_index = goodent2(FINAL);
 end
 
 function imgF = rgb2hue(img)
